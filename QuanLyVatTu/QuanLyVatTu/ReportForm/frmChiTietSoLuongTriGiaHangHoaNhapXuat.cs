b9511497b2d83e247ffbf23410f828eb6d1d3924
your name <412ngan@gmail.com>
2024-06-05 13:53:05 +0700

    sua giao dien

--- QuanLyVatTu/QuanLyVatTu/ReportForm/frmChiTietSoLuongTriGiaHangHoaNhapXuat.cs
+++ QuanLyVatTu/QuanLyVatTu/ReportForm/frmChiTietSoLuongTriGiaHangHoaNhapXuat.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DevExpress.XtraReports.UI;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +37,78 @@ namespace QuanLyVatTu.ReportForm
         private void dteToiNgay_EditValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void frmChiTietSoLuongTriGiaHangHoaNhapXuat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXemTruoc_Click(object sender, EventArgs e)
+        {
+
+
+            string vaiTro = Program.role;
+            string loaiPhieu = (cboLoaiPhieu.SelectedItem.ToString() == "NHAP") ? "NHAP" : "XUAT";
+
+            DateTime fromDate = dteTuNgay.DateTime;
+            DateTime toDate = dteToiNgay.DateTime;
+            ReportChiTietSoLuongTriGiaHangHoa report = new ReportChiTietSoLuongTriGiaHangHoa(vaiTro, loaiPhieu, fromDate, toDate);
+            /*GAN TEN CHI NHANH CHO BAO CAO*/
+            report.txtLoaiPhieu.Text = cboLoaiPhieu.SelectedItem.ToString().ToUpper();
+            report.txtTuNgay.Text = fromDate.ToString("dd-MM-yyyy");
+            report.txtToiNgay.Text = toDate.ToString("dd-MM-yyyy");
+            ReportPrintTool printTool = new ReportPrintTool(report);
+            printTool.ShowPreviewDialog();
+        }
+
+        private void btnXuatBan_Click(object sender, EventArgs e)
+        {
+            string vaiTro = Program.role;
+            string loaiPhieu = (cboLoaiPhieu.SelectedItem.ToString() == "NHAP") ? "NHAP" : "XUAT";
+
+            try
+            {
+
+                DateTime fromDate = dteTuNgay.DateTime;
+                DateTime toDate = dteToiNgay.DateTime;
+
+
+
+                ReportChiTietSoLuongTriGiaHangHoa report = new ReportChiTietSoLuongTriGiaHangHoa(vaiTro, loaiPhieu, fromDate, toDate);
+
+
+                /*GAN TEN CHI NHANH CHO BAO CAO*/
+                report.txtLoaiPhieu.Text = cboLoaiPhieu.SelectedItem.ToString().ToUpper();
+                report.txtTuNgay.Text = fromDate.ToString("dd-MM-yyyy");
+                report.txtToiNgay.Text = toDate.ToString("dd-MM-yyyy");
+
+
+                if (File.Exists(@"D:\ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf"))
+                {
+                    DialogResult dr = MessageBox.Show("File ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf tại ổ D đã có!\nBạn có muốn tạo lại?",
+                        "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (dr == DialogResult.Yes)
+                    {
+                        report.ExportToPdf(@"D:\ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf");
+                        MessageBox.Show("File ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf đã được ghi thành công tại ổ D",
+                "Xác nhận", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                }
+                else
+                {
+                    report.ExportToPdf(@"D:\ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf");
+                    MessageBox.Show("File ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf đã được ghi thành công tại ổ D",
+                "Xác nhận", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Vui lòng đóng file ReportChiTietSoLuongTriGiaHangHoaNhapXuat.pdf",
+                    "Xác nhận", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                return;
+            }
         }
     }
 }
